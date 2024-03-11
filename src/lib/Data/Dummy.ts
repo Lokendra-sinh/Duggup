04bdd8203799202aec3da6108945a259e93e51ca
Lokendra-sinh <anizen242@gmail.com>
2024-03-11 16:12:00 +0530

    removed .env file

--- src/lib/Data/Dummy.ts
+++ src/lib/Data/Dummy.ts
@@ -1,7 +1,8 @@
 import type { Timeline } from "../Types/Timeline";
 
-const DUGGUP_ICON = import.meta.env.VITE_DUGGUP_ICON;
-const BETTERUP_ICON = import.meta.env.VITE_BETTERUP_ICON;
+const DUGGUP_ICON="https://s3-alpha-sig.figma.com/img/f1bc/3eca/3db6ec41cda31968010a7f56f3c3658a?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FT4oZFOFyVS5aS~O~42xm38C0rnk~5fKs4MDSsflPi3Zxlf~4iKBbwUOtYMGPRLGF5jLph723jRvd5p1sJ2nZ7UAfNt2cRfal~5Vot0EDZFR5vHxpzDPqBAk5I3nCXqorDNl1-nvLQw8J~bThtOy4km1ADWoGFr3ufCrHP7NakEPdDAFfh0f6b7K8sjaK3-G2BB0yTFOvFYCsCHvyXOBOfOh~fqEZQxyObR2trfFgLR2cabLauALtH6e6srK393GL9Cf7UHQ17IbXQdP18iQfif1a7vrosu4Xmf4bVGi-27Ic8YghSJWXJ71nXX54CtGvhPSTY-di5B8zIWBrxvEmw__"
+const BETTERUP_ICON="https://s3-alpha-sig.figma.com/img/1b23/f02f/60bc556cd22a575549ce43ad307ec0ee?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XmlN~3FGwC3Eiofaiw2YRoSilaWLnmZbaz6AJ8ZSghKKIrxdpbwyo8DbmfqII9iJCL5u4Qtb4ssMaMo1owhn7V6f0vsv3yLSbO8YgEPtwxlwwVEwbeVCXMMvyfSjT-3G20lXcqSXNkS5s5pZ8waU-oYL9e7KJ3Jg1QkR1R5KzLrk~pRCSBDSLMes4gfPUB9x-0~z2O8bdCaHJfIJnZd~E7ib1MYrKIya6vLwtmCWgaVnIAItzAeQiILyqUVNL9QvDQvoNeoISDDgLyM1D6zhql6HVe6k3soojKRXQ~mby04ys4EnmtUd2wNYkhQE~nnr9U6WY1tGxt7CmM05hiP47w__"
+
 
 
 export let timelineItems: Timeline[] = [
